df313bca0333e6e92579036cdf1bab9e50902ceb
Kyle John <njk230@gmail.com>
2017-04-18 09:32:27 +0900

    Add Id to ToggleButton

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ToggleButton;
+
 public class Controller {
+    @FXML
+    private ToggleButton OnOffToggleButton;
+
+    @FXML
+    protected void handleButtonAction(ActionEvent event) {
+        boolean isOn = OnOffToggleButton.isSelected();
+
+        new Alert(Alert.AlertType.INFORMATION, isOn ? "is On" : "is Off").show();
+    }
 }
