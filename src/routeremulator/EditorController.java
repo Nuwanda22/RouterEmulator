e3f5b71f9e6c195c9040c90c420218db75594e64
Kyle John <njk230@gmail.com>
2017-04-25 10:47:50 +0900

    Add Editor Page

--- src/routeremulator/EditorController.java
+++ src/routeremulator/EditorController.java
@@ -1,8 +1,38 @@
 package routeremulator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
 /**
  * Created by Nuwanda on 2017-04-19.
  */
 public class EditorController {
+    @FXML
+    private Button OKButton;
+    @FXML
+    private TextField SsidTextField;
+    @FXML
+    private TextField PasswordTextField;
 
+    private MainController controller;
+    public void setController(MainController controller) {
+        this.controller = controller;
+
+        SsidTextField.setText(controller.getSsid());
+        PasswordTextField.setText(controller.getPassword());
+    }
+
+    @FXML
+    protected void handleOKButtonAction(ActionEvent event) {
+        if(event.getTarget() == OKButton) {
+            controller.updateInformation(SsidTextField.getText(), PasswordTextField.getText());
+        }
+
+        StackPane root = (StackPane) OKButton.getScene().getRoot();
+        root.getChildren().remove(1);
+    }
 }
